e4e52140e4728cf7984fce58dce32ebcc51ff646
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-02-10 17:56:14 -0300

    get /

--- src/api/app.js
+++ src/api/app.js
@@ -8,6 +8,8 @@ const errorMiddleware = require('../middlewares/error');
 const app = express();
 app.use(cors());
 
+app.get('/', rescue(async (req, res) => res.status(200).json({ message: 'TA FUNCIONANDO!!' })));
+
 app.get('/users', rescue(UserController));
 
 app.use(errorMiddleware);
